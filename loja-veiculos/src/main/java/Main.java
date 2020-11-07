21ab2a8d1296bbb6cd5b48550fbb3110f098b972
elian <elian@192.168.0.19>
2020-11-07 12:20:27 -0300

    Projeto Loja de Veiculos

---
+++ loja-veiculos/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 
+			EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("AlgaWorks-PU");
+			entityManagerFactory.close();
+			}// TODO Auto-generated method stub
+
+	}
+
+ 
